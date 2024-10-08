0ef1e76b062aa5506c449fe82ef108910cd9478b
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2024-10-08 21:57:57 +0300

    add social media links

--- apps/nextjs-app/src/pages/index.tsx
+++ apps/nextjs-app/src/pages/index.tsx
@@ -1,8 +1,36 @@
+import {Icon} from "@iconify/react";
+
 import {ReactComponent as LogomarkLarge} from "../../assets/logomark.svg";
 
 export const Index = () => (
     <main className={"flex min-h-[100svh] p-16"}>
-        <LogomarkLarge className={"m-auto"} role={"decoration"} />
+        <div className={"m-auto flex flex-col gap-8"}>
+            <LogomarkLarge
+                aria-hidden={"true"}
+                className={"h-full max-h-[320px] w-full max-w-[320px]"}
+            />
+            <div className={"flex justify-center gap-4"}>
+                <a
+                    href={"https://linkedin.com/in/danielgiljam"}
+                    rel={"noreferrer"}
+                    target={"_blank"}
+                >
+                    <Icon
+                        className={"h-8 w-8"}
+                        icon={"simple-icons:linkedin"}
+                    />
+                    <span className={"sr-only"}>LinkedIn</span>
+                </a>
+                <a
+                    href={"https://github.com/danielgiljam"}
+                    rel={"noreferrer"}
+                    target={"_blank"}
+                >
+                    <Icon className={"h-8 w-8"} icon={"simple-icons:github"} />
+                    <span className={"sr-only"}>GitHub</span>
+                </a>
+            </div>
+        </div>
     </main>
 );
 
