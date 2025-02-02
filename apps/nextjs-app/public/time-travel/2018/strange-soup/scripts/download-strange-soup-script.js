5ce44edc1090721111e95b90375aab8b279e7cfd
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-02-02 17:24:13 +0200

    fix links

--- apps/nextjs-app/public/time-travel/2018/strange-soup/scripts/download-strange-soup-script.js
+++ apps/nextjs-app/public/time-travel/2018/strange-soup/scripts/download-strange-soup-script.js
@@ -11,14 +11,14 @@ switch (clientOS) {
     case "Windows":
         document.getElementById("paragraph-2").setAttribute("style", "display: block");
         console.log("Initiating download of strange-soup-installer.exe...");
-        document.getElementById("download-trigger").setAttribute("src", "binaries/strange-soup-installer.exe");
+        document.getElementById("download-trigger").setAttribute("src", "/time-travel/2018/strange-soup/binaries/strange-soup-installer.exe");
         break;
     case "Mac OS":
         document.getElementById("paragraph-2").setAttribute("style", "display: block");
         console.log("Initiating download of strange-soup-installer.dmg...");
-        document.getElementById("download-trigger").setAttribute("src", "binaries/strange-soup-installer.dmg");
+        document.getElementById("download-trigger").setAttribute("src", "/time-travel/2018/strange-soup/binaries/strange-soup-installer.dmg");
         break;
     default:
-        document.getElementById("strange-soup-image").setAttribute("src", "media-content/strange-soup-image-1.png");
+        document.getElementById("strange-soup-image").setAttribute("src", "/time-travel/2018/strange-soup/media-content/strange-soup-image-1.png");
         document.getElementById("paragraph-1").setAttribute("style", "display: block");
 }
