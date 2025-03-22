57a08814d659a7092e8b81bee21bc184917627b0
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-03-22 12:12:55 +0200

    remove log in and get started nav items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,19 +16,7 @@ export const Header = () => {
               Flowbite
             </span>
           </a>
-          <div className="flex items-center lg:order-2">
-            <a
-              href="#"
-              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-            >
-              Log in
-            </a>
-            <a
-              href="#"
-              className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-            >
-              Get started
-            </a>
+          <div className="flex items-center">
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
@@ -64,7 +52,7 @@ export const Header = () => {
             </button>
           </div>
           <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto"
             id="mobile-menu-2"
           >
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
