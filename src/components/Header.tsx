5b6341e20d40f51ddae67392d828694e36a53e09
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-03-24 09:29:42 +0200

    add Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Logo } from "./Logo";
+
 const navItems = [
   { label: "Blog", href: "#" },
   { label: "About", href: "#" },
@@ -11,15 +13,13 @@ export const Header = () => {
     <header>
       <nav className="border-gray-200 bg-white px-4 py-2.5 lg:px-6 dark:bg-gray-800">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-          <a href="https://flowbite.com" className="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
+          <a href="/" className="flex items-center">
+            <Logo
+              className="h-8"
+              variant="medium_modified"
+              aria-hidden="true"
             />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
+            <span className="sr-only">Daniel Giljam</span>
           </a>
           <div className="flex items-center">
             <button
