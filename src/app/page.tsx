cb8ac5292823181d3ea9e8c4fa255835f256bb5a
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-03-26 15:15:13 +0200

    remove everything related to posts/ for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,14 +34,14 @@ export default function Home() {
   return (
     <>
       <LandingPageHero />
-      <div className="mx-auto max-w-xl py-8">
+      {/* <div className="mx-auto max-w-xl py-8">
         <h1 className="mb-8 text-center text-2xl font-black">
           Next.js + Contentlayer Example
         </h1>
         {posts.map((post, idx) => (
           <PostCard key={idx} {...post} />
         ))}
-      </div>
+      </div> */}
     </>
   );
 }
