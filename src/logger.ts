5b24d040aea28761631a867f0b528c435ebd8d60
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-04-01 10:38:04 +0300

    setup adze (logging library)

---
+++ src/logger.ts
@@ -0,0 +1,32 @@
+import adze, { setup, levels, type Level } from "adze";
+
+const withinRange = (x: number, min: number, max: number) =>
+  x >= min && x <= max;
+
+const isLogLevel = (loglevel: unknown): loglevel is Level | number =>
+  levels.includes(loglevel as never) || withinRange(Number(loglevel), 0, 8);
+
+const getLogLevel = (): Level | number => {
+  if (typeof localStorage !== "undefined") {
+    const loglevelFromLocalStorage = localStorage.getItem("loglevel");
+    if (isLogLevel(loglevelFromLocalStorage)) {
+      return loglevelFromLocalStorage;
+    }
+  }
+  if (isLogLevel(process.env.NEXT_PUBLIC_LOGLEVEL)) {
+    return process.env.NEXT_PUBLIC_LOGLEVEL;
+  }
+  if (isLogLevel(process.env.LOGLEVEL)) {
+    return process.env.LOGLEVEL;
+  }
+  if (process.env.NODE_ENV === "development") {
+    return "debug";
+  }
+  return "log";
+};
+
+export const globalStore = setup({
+  activeLevel: getLogLevel(),
+});
+
+export const globalLogger = adze;
