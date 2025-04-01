f7b2ebe2d5c6a216736837d1743e947c205756ab
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-04-01 12:41:40 +0300

    improve eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-config-prettier";
+import unicorn from "eslint-plugin-unicorn";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,7 +14,146 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
-    ignores: ["public/time-travel"],
+    ignores: [".contentlayer", ".next", "public/time-travel", "next-env.d.ts"],
+  },
+  unicorn.configs.all,
+  prettier,
+  {
+    files: [
+      "**/*.ts",
+      "**/*.mts",
+      "**/*.tsx",
+      "**/*.js",
+      "**/*.mjs",
+      "**/*.jsx",
+    ],
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
+    rules: {
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+          caughtErrors: "all",
+        },
+      ],
+      "func-names": ["error", "always"],
+      "func-name-matching": [
+        "error",
+        "always",
+        {
+          considerPropertyDescriptor: true,
+        },
+      ],
+      "func-style": ["error", "expression"],
+      "import/consistent-type-specifier-style": ["error", "prefer-inline"],
+      "import/no-extraneous-dependencies": ["error"],
+      "import/no-useless-path-segments": [
+        "error",
+        {
+          noUselessIndex: true,
+        },
+      ],
+      "import/order": [
+        "warn",
+        {
+          pathGroupsExcludedImportTypes: ["unknown"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          paths: [
+            {
+              name: "react",
+              allowImportNames: ["default"],
+              message: "Only React default import is allowed.",
+            },
+          ],
+          patterns: [
+            {
+              regex: String.raw`(\.{1,2}\/)(\.{2}\/)*(\.+[^./]+[^/]*|[^./][^/]*)\/`,
+              message: "Deep relative imports are not allowed.",
+            },
+          ],
+        },
+      ],
+      quotes: [
+        "error",
+        "double",
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: false,
+        },
+      ],
+      "sort-imports": [
+        "error",
+        {
+          ignoreDeclarationSort: true,
+          memberSyntaxSortOrder: ["all", "single", "multiple", "none"],
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          cases: {
+            camelCase: true,
+            pascalCase: true,
+          },
+        },
+      ],
+      "unicorn/no-null": "off",
+      "unicorn/no-keyword-prefix": "off",
+      "unicorn/prefer-json-parse-buffer": "off",
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          replacements: {
+            args: false,
+            props: false,
+            ref: false,
+          },
+          allowList: {
+            generateStaticParams: true,
+            urlSearchParams: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.tsx", "**/*.jsx"],
+    rules: {
+      "react/jsx-curly-brace-presence": [
+        "warn",
+        {
+          props: "always",
+          children: "never",
+        },
+      ],
+      "react/jsx-sort-props": [
+        "warn",
+        {
+          callbacksLast: true,
+          shorthandLast: true,
+          ignoreCase: true,
+          reservedFirst: true,
+        },
+      ],
+    },
   },
 ];
 
