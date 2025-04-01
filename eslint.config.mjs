d7b4d1545d22f8173b7d71ed74ca142a56f8f652
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-04-01 11:51:29 +0300

    fix eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
-    ignores: "public/time-travel",
+    ignores: ["public/time-travel"],
   },
 ];
 
