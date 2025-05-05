b4560e68be2e728d9bde24736b9f498dc2116a5b
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2025-05-05 08:40:31 +0300

    unnecessary to validate referrers

--- src/app/api/redirect/route.ts
+++ src/app/api/redirect/route.ts
@@ -1,13 +1,10 @@
 import { type NextRequest, NextResponse } from "next/server";
 
 const allowedUrlBases = ["https://github.com/danielgiljam/slides"];
-const allowedReferrers = new Set(["meetabit"]);
 
 export const GET = (request: NextRequest) => {
   const to = request.nextUrl.searchParams.get("to");
-  const referrer = request.nextUrl.searchParams.get("referrer");
-  return allowedUrlBases.some((urlBase) => to?.startsWith(urlBase)) &&
-    allowedReferrers.has(referrer as never)
+  return allowedUrlBases.some((urlBase) => to?.startsWith(urlBase))
     ? NextResponse.redirect(to!)
     : new NextResponse(undefined, { status: 400 });
 };
